557800b6f14e2ed6b573bb468d250f1b74982111
Haru-Kazumoto <jiaathaya@gmail.com>
2025-12-22 20:40:13 +0700

    add: comment

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -13,6 +13,22 @@ class AuthController extends Controller {
             $error = 'Email atau password salah';
         }
         
+        /**
+         * view punya 3 parameter sekarang
+         * 1. nama view
+         * 2. data yang dikirim ke view
+         * 3. layout yang dipakai
+         * default layout adalah 'user'
+         * kita mau ganti layoutnya jadi 'student'
+         * karena halaman login ini untuk student
+         * jadi kita panggil layout student
+         * jangan lupa buat layout student di views/layouts/student.php
+         * dan isinya bisa di copy dari layout guest
+         * 
+         * jadi layout itu utamanya isian dari header, footer, dan navigasi
+         * biar gak perlu nulis berulang-ulang di tiap view
+         * jadi di layout kita panggil $content yang isinya adalah view yang kita panggil
+         */
         $this->view('login', ['error' => $error], 'student');
     }
 }
